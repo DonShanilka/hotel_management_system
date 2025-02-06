aa1ad5c765a4df9b43e5a002f09a7e6731f72feb
Shanilka800@# <shanilka799@gmail.com>
2025-02-06 14:52:20 +0530

    cretae accusationSlice

--- src/reducer/Accusation.ts
+++ src/reducer/Accusation.ts
@@ -1,1 +1,25 @@
-import 
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const accusationSlice = createSlice({
+  name: "accusation",
+  initialState: initialState,
+  reducers: {
+    addAccusation(state, action) {
+      state.push(action.payload);
+    },
+    updateAccusation: (state, action) => {
+      const { id, accusation } = action.payload;
+      const accusationIndex = state.findIndex(
+        (accusation) => accusation.id === id
+      );
+      if (accusationIndex !== -1) {
+        state[accusationIndex] = accusation;
+      }
+    },
+    deleteAccusation: (state, action) => {
+      return state.filter((accusation) => accusation.id !== action.payload.id);
+    },
+  },
+});
