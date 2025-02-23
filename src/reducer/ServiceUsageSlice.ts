900fe03426d1bc7c27e22d2b2a3af451f254d527
Shanilka800@# <shanilka799@gmail.com>
2025-02-23 20:59:10 +0530

    create extraReducers in UsageSlice

--- src/reducer/ServiceUsageSlice.ts
+++ src/reducer/ServiceUsageSlice.ts
@@ -1,6 +1,7 @@
 import { ServiceUsage } from "../model/ServiceUsage.ts";
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import axios from "axios";
+import {deleteEmployee, getallEmployee, saveEmployee, updateEmployee} from "./EmployeeSlice.ts";
 
 const initialState:ServiceUsage[] = [];
 
@@ -82,6 +83,50 @@ const serviceUsageSlice = createSlice({
     //   return state.filter((accusation) => accusation.id !== action.payload.id);
     // },
   },
+  extraReducers:(builder)=>{
+    builder
+        .addCase(saveUsage.fulfilled,(state,action)=>{
+          state.push(action.payload);
+          console.log("Usage saved")
+        })
+        .addCase(saveUsage.rejected,(state,action)=>{
+          console.log("Usage Saved Rejected :",action.payload)
+        })
+        .addCase(saveUsage.pending,()=>{
+          console.log("Usage saving pending")
+        })
+
+    builder
+        .addCase(updateUsage.fulfilled, (state, action) => {
+          const index = state.findIndex(usage => usage.usageID === action.payload.usageID);
+          if (index !== -1) {
+            state[index] = action.payload;
+          }
+          console.log("Usage Updated");
+        })
+        .addCase(updateUsage.rejected, (state, action) => {
+          console.log("Failed to Usage Employee: ", action.error);
+        })
+
+    builder
+        .addCase(deleteUsage.fulfilled,(state,action)=>{
+          return state.filter(usage => usage.usageID !== action.payload);
+        })
+        .addCase(deleteUsage.rejected,(state,action)=> {
+          console.log("Failed to delete Usage : ", action.payload)
+        })
+
+    builder
+        .addCase(getallUsage.fulfilled,(state,action)=> {
+          return action.payload;
+        })
+        .addCase(getallUsage.rejected,(state,action)=> {
+          console.log("Failed to get Usage :", action.payload)
+        })
+        .addCase(getallUsage.pending,()=>{
+          console.log("Fetching Usage ....")
+        })
+  }
 });
 
 export default serviceUsageSlice.reducer;
