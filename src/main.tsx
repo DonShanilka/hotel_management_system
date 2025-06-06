dec67fa06575639ab566a1d79ee1403b8fc0923c
DonShanilka <shanilka799@gmail.com>
2025-06-06 19:16:56 +0530

    remove commented-out code for cleaner structure

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ createRoot(document.getElementById('root')!).render(
     {/* <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold text-blue-500">Hello Shanilka</h1>
     </div> */}
-  </StrictMode>,
+  </StrictMode>
 )
